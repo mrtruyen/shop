1fde72c13913b0b1a1daa43781dfb4f4d88cd28b
Thanh Truyen <mrtruyen.info@gmail.com>
2015-11-09 10:02:21 +0700

    First Upload

---
+++ libraries/config.php
@@ -0,0 +1,9 @@
+<?php
+	class config{
+		const HOST_NAME = "localhost";
+		const HOST_USER = "root";
+		const HOST_PASS = "";
+		const DB_NAME	= "shop";
+	}
+	define("base_url","http://shopmaytinh.com/");
+?>
